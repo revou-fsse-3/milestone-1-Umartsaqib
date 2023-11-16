4f638f76332496b13f05a73ba4ee32a22555ff37
Umartsaqib <umartsqb@gmail.com>
2023-11-16 17:42:42 +0700

    project milestone 1

---
+++ index.js
@@ -0,0 +1,26 @@
+const isNightMode = localStorage.getItem('nightMode');
+
+if (isNightMode === 'enabled') {
+  enableNightMode();
+}
+
+function enableNightMode() {
+  const body = document.body;
+  body.classList.add('night-mode');
+  localStorage.setItem('nightMode', 'enabled');
+}
+
+function disableNightMode() {
+  const body = document.body;
+  body.classList.remove('night-mode');
+  localStorage.setItem('nightMode', 'disabled');
+}
+
+document.getElementById('night-mode-button').addEventListener('click', function() {
+  const isNightModeEnabled = document.body.classList.contains('night-mode');
+  if (isNightModeEnabled) {
+    disableNightMode();
+  } else {
+    enableNightMode();
+  }
+});
