96f8e9351da44b9748894659da8f4d778107cd97
Umartsaqib <umartsqb@gmail.com>
2023-11-17 03:09:50 +0700

    project milestone 1 fixed responsive design

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const isNightMode = localStorage.getItem('nightMode');
+// const isNightMode = localStorage.getItem('nightMode');
 
-if (isNightMode === 'enabled') {
-  enableNightMode();
-}
+// if (isNightMode === 'enabled') {
+//   enableNightMode();
+// }
 
-function enableNightMode() {
-  const body = document.body;
-  body.classList.add('night-mode');
-  localStorage.setItem('nightMode', 'enabled');
-}
+// function enableNightMode() {
+//   const body = document.body;
+//   body.classList.add('night-mode');
+//   localStorage.setItem('nightMode', 'enabled');
+// }
 
-function disableNightMode() {
-  const body = document.body;
-  body.classList.remove('night-mode');
-  localStorage.setItem('nightMode', 'disabled');
-}
+// function disableNightMode() {
+//   const body = document.body;
+//   body.classList.remove('night-mode');
+//   localStorage.setItem('nightMode', 'disabled');
+// }
 
-document.getElementById('night-mode-button').addEventListener('click', function() {
-  const isNightModeEnabled = document.body.classList.contains('night-mode');
-  if (isNightModeEnabled) {
-    disableNightMode();
+// document.getElementById('night-mode-button').addEventListener('click', function() {
+//   const isNightModeEnabled = document.body.classList.contains('night-mode');
+//   if (isNightModeEnabled) {
+//     disableNightMode();
+//   } else {
+//     enableNightMode();
+//   }
+// });
+
+function myFunction() {
+  var x = document.getElementById("mytopnav");
+  if (x.className === "nav-menu") {
+    x.className += " responsive";
   } else {
-    enableNightMode();
+    x.className = "nav-menu";
   }
-});
+}
